794512e27e220b7c8605a6cbf18a8b41ea3cc967
Victoria Kovaliova <victkovaliova@gmail.com>
2022-08-08 15:18:32 +0300

    75 redux-form

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -5,6 +5,7 @@ import dialogsReducer from './dialogs-reducer';
 import sidebarReducer from './sidebar-reducer';
 import usersReducer from './users-reducer';
 import authReducer from './auth-reducer';
+import { reducer as formReducer } from 'redux-form';
 
 let rootReducer = combineReducers({
     profilePage: profileReducer,
@@ -12,6 +13,7 @@ let rootReducer = combineReducers({
     sidebar: sidebarReducer,
     usersPage: usersReducer,
     auth: authReducer,
+    form: formReducer,
 })
 let store = createStore(rootReducer, applyMiddleware(thunk));
 
